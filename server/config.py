eb4b0f7f545d11c9b06793d4a695f266065cd59e
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-09 12:45:48 +0300

    Setup config.py

--- server/config.py
+++ server/config.py
@@ -1,1 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+
+app = Flask(__name__)
+
+app.secret_key = "qwertyuiopasdfghjkl"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.json.compact = False
+
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+db.init_app(app)
