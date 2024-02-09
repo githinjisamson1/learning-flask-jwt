c2fa86363ac9eb3df4880b7d95028962616dfaef
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-09 13:23:32 +0300

    Register user_bp

--- server/app.py
+++ server/app.py
@@ -1,13 +1,7 @@
-from config import app, api
-from flask_restful import Resource
+from config import app
+from controllers.user_controllers import user_bp
 
-
-class Index(Resource):
-    def get(self):
-        return {"success": True, "message": "Hello World"}, 200
-
-
-api.add_resource(Index, "/")
+app.register_blueprint(user_bp, )
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
