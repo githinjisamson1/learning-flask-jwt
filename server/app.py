6e29d188162e3afe3890c21da6080e556d180263
githinjisamson1 <githinjisamson148@gmail.com>
2024-02-11 05:58:52 +0300

    Register auth_bp

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
 from config import app
 from controllers.user_controllers import user_bp
+from controllers.auth_controllers import auth_bp
 
 
 # will register blueprints here
 app.register_blueprint(user_bp)
+app.register_blueprint(auth_bp, url_prefix="auth/")
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
