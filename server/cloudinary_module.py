00a0c2e2677293d603f61b113767edd2a459c02b
githinjisamson1 <githinjisamson148@gmail.com>
2024-03-15 05:28:50 +0300

    Set up reqparse

--- server/cloudinary_module.py
+++ server/cloudinary_module.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template, request
 from cloudinary import CloudinaryUploader, config
 from dotenv import load_dotenv
 from os import environ
+from flask_restful import reqparse, Api, Resource
 
 load_dotenv()
 
 app = Flask(__name__)
+
+parser = reqparse.RequestParser()
+parser.add_argument('file', type=str, help='File to upload')
 
 # Configure Cloudinary
 config(
